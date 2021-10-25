0134b6a97c4974d616718a544c455cdfedbf6149
Kumar Anirudha <mail@anirudha.dev>
2021-10-25 13:52:07 +0530

    added simple args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("args: {:?}", args);
+
+    let a1: &String = &args[1];
+    let a2: &String = &args[2];
+    println!("arg 1 {}", a1);
+    println!("arg 2 {}", a2);
 }
