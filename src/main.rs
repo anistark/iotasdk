d9b11e2f14fd0cc7c12e3550268b3ae1b37e9e23
Kumar Anirudha <mail@anirudha.dev>
2021-11-01 15:44:00 +0530

    trying to subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,67 @@
+// use clap::{AppSettings, Parser};
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-
-#[clap(name = "IOTASDK")]
-#[clap(version = "0.0.1")]
-#[clap(author = "Kumar Anirudha github.com/anistark")]
-#[clap(about = "Simplifying BUIDL using IOTA!")]
+#[derive(Parser)]
+#[clap(version = "0.0.1", author = "Kumar Anirudha. <mail@anirudha.dev>")]
 
 struct Opts {
-    /// Command to run
-    #[clap(short, long)]
-    command: String,
+    // /// Sets a custom config file. Could have been an Option<T> with no default too
+    // #[clap(short, long, default_value = "default.conf")]
+    // config: String,
 
-    /// Number of times to greet
-    #[clap(long, default_value = "1")]
-    count: u8,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: String,
+
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    #[clap(version = "0.0.1", author = "Kumar Anirudha <mail@anirudha.dev>")]
+
+    Test(Test),
+}
+
+/// A subcommand for controlling testing
+#[derive(Parser)]
+struct Test {
+    /// Print debug info
+    #[clap(short)]
+    debug: bool
 }
 
 fn main() {
-    let options = Opts::parse();
+    let opts: Opts = Opts::parse();
 
-    for n in 0..options.count {
-        println!("Command is {} for count {}!", options.command, n+1)
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    // println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input);
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        _ => println!("Don't be ridiculous"),
     }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Test(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
+
+    // more program logic goes here...
 }
