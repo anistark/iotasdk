1f0e4a964ef5cb1ef74f6f39836dd5b36aafcabd
Kumar Anirudha <mail@anirudha.dev>
2021-11-05 22:48:26 +0530

    cleaning up

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-// use clap::{AppSettings, Parser};
+#[macro_use]
+extern crate clap;
+
 use clap::Parser;
 
 #[derive(Parser)]
 
-#[clap(name = "IOTASDK")]
-#[clap(version = "0.0.1")]
-#[clap(author = "Kumar Anirudha github.com/anistark")]
-#[clap(about = "Simplifying BUIDL using IOTA!")]
+#[clap(name = crate_name!())]
+#[clap(version = crate_version!())]
+#[clap(author = crate_authors!())]
+#[clap(about = crate_description!())]
 
 struct Opts {
-    // /// Sets a custom config file. Could have been an Option<T> with no default too
+    /// Sets a custom config file. Could have been an Option<T> with no default too
     #[clap(short, long, default_value = "default.conf")]
     config: String,
 
@@ -28,26 +30,34 @@ struct Opts {
 enum SubCommand {
     #[clap(version = "0.0.1", author = "Kumar Anirudha <mail@anirudha.dev>")]
 
-    Test(Test),
+    Node(Node),
+
+    Init(Init),
 }
 
-/// A subcommand for controlling testing
+/// Configure and Control your IOTA Nodes
 #[derive(Parser)]
-struct Test {
+struct Node {
     /// Print debug info
     #[clap(short)]
     debug: bool
 }
 
+/// Initialise IOTASDK
+#[derive(Parser)]
+struct Init {
+    /// Config File
+    #[clap(short, long, default_value="default.conf")]
+    config: String
+
+    // Other config here
+}
+
 fn main() {
     let opts: Opts = Opts::parse();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    // println!("Value for config: {}", opts.config);
     println!("Using input file: {}", opts.input);
 
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
     match opts.verbose {
         0 => println!("No verbose info"),
         1 => println!("Some verbose info"),
@@ -55,17 +65,18 @@ fn main() {
         _ => println!("Don't be ridiculous"),
     }
 
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
     match opts.subcmd {
-        SubCommand::Test(t) => {
+        SubCommand::Node(t) => {
             if t.debug {
                 println!("Printing debug info...");
             } else {
                 println!("Printing normally...");
             }
         }
+
+        SubCommand::Init(_i) => {
+            println!("Printing config info... {}", _i.config);
+        }
     }
 
-    // more program logic goes here...
 }
