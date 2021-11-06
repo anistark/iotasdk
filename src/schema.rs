8113030f7a8b3b7d0ad4cbe0b26680865d4945b8
Kumar Anirudha <mail@anirudha.dev>
2021-11-07 01:40:25 +0530

    updated schema init. removed file dependency

--- src/schema.rs
+++ src/schema.rs
@@ -4,7 +4,33 @@ use std::fs;
 pub fn init(name: String) {
     println!("Initialising Schema Tool for new smart contract {}...", name);
     // Create schema.yaml file with initial content.
-    let data = fs::read_to_string("./static/schema-sample.yaml").expect("Unable to read file").replace("ContractName", &name);
+    //TODO: Generate Code Instead of copy pasting.
+    let data = "name: ContractName
+    description: My awesome ContractName
+    structs: {}
+    typedefs:
+      AllowancesForAgent: map[AgentID]Int64
+    state:
+      allAllowances=a: map[AgentID]AllowancesForAgent
+      balances=b: map[AgentID]Int64 // balances per account
+      supply=s: Int64 // total supply of the token
+    funcs:
+      init:
+        params:
+          creator: AgentID // creator/owner of the initial supply
+      transfer:
+        params:
+          account=ac: AgentID // target account
+          amount=am: Int64 // amount of tokens to transfer
+    views:
+      balanceOf:
+        params:
+          account=ac: AgentID // sender account
+        results:
+          amount=am: Int64
+      totalSupply:
+        results:
+          supply=s: Int64".replace("ContractName", &name);
     fs::write("schema.yaml", data).expect("Unable to write file");
     println!("Your new schema is now ready! Build something awesome.")
 }
